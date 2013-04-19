294b5ccd274f78230bb2c6e128785d0e70305698
Jonas Stein <jonas.stein@uni-koeln.de>
2013-04-19 21:18:04 +0200

    init

---
+++ fab.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*- 
+
+__version__ = 0.01 
+__author__ = "Jonas Stein"
+__contributors__ = ""
+
+"""Description:
+""" 
+
+import sqlite
+
+
+
+if __name__ == "__main__":
+    main()
