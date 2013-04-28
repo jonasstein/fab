393b1a448154113783e43db49305b5140aed6e7e
Ignacio Vergara Kausel <ivergarakausel@gmail.com>
2013-04-28 13:35:48 +0200

    Cleaning, unittest mockups, basic DB operations.

--- fab.py
+++ fab.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-#  -*- coding: utf-8 -*- 
+#  -*- coding: utf-8 -*-
 
-__version__ = 0.01 
+__version__ = 0.01
 __author__ = "Jonas Stein"
 __contributors__ = "Ignacio Vergara"
 
 """Description:
-""" 
+"""
 
 from fsql import FabDatabase
 
@@ -23,30 +23,30 @@ class Data(object):
 
 	def __init__(self):
 		pass
-		
+
 
 class Contact(object):
 	"""Class abstracting the whole contact information.
 	It is composed by a set/list of Data object instances
 	representing all the pertinent entries for a contact."""
-	
-	self.ID = None	
+
+	self.ID = None
 	self.entries = []
 
 	def __init__(self):
 		pass
-	
+
 	def load(self):
 		"""Loads all the entries for the given contact."""
 		pass
-	
+
 	def delete(self):
 		"""Removes the whole contact from the DB."""
 		pass
-	
-	
+
+
 def main():
-    fdb = FabDatabase("fab.db") 
+    fdb = FabDatabase("fab.db")
     fdb.init_table()
 
 if __name__ == "__main__":
--- fsql.py
+++ fsql.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
-#  -*- coding: utf-8 -*- 
+#  -*- coding: utf-8 -*-
 
 """Description: fsql module
 Handles low level DB instructions.
-""" 
+"""
 
 import sqlite3
 import sys
+
+import unittest
 
 class FabDatabase:
     """Class uppercase first letter, methods lowercase, underscore
     """
 
     def __init__(self, filename):
-        try: 
+        try:
             self.conn = sqlite3.connect(filename)
         except RuntimeError:
             print("Runtime Error: Could not create new database.")
@@ -27,7 +29,7 @@ class FabDatabase:
         """create new table, if there was no db before
         """
         try:
-            self.conn.execute('''CREATE TABLE IF NOT EXISTS data(
+            self.conn.execute("CREATE TABLE IF NOT EXISTS data(
 								ID INTEGER PRIMARY KEY,
 								UserID INTEGER,
 								Type TEXT,
@@ -35,9 +37,9 @@ class FabDatabase:
 								Priority INTEGER,
 								Value TEXT,
 								TimeStamp TEXT,
-								Comment TEXT)''') 
-								
-        except sqlite3.OperationalError:    
+								Comment TEXT)")
+
+        except sqlite3.OperationalError:
             print("OperationalError")
         except sqlite3.DatabaseError:
             print("DatabaseError: file is encrypted or is not a database")
@@ -45,13 +47,17 @@ class FabDatabase:
             print "Unexpected error:", sys.exc_info()[0]
             raise
 
-    def store_row(self):
+    def store_row(self, registry):
+        """Inserts a registry from the data table.
         """
-        """
+        self.conn.execute("INSERT INTO data VALUES (?,?,?,?,?,?,?)", registry)
+        self.conn.commit()
 
-    def erase_row(self):
+    def erase_row(self,ID):
+        """Erases a row.
         """
-        """
+        self.conn.execute( "DELETE from data where ID = ? ", ID)
+        self.conn.commmit()
 
     def read(self):
         """given a set of attr. and ID's return such attr. of ID's
@@ -60,12 +66,28 @@ class FabDatabase:
     def search(self):
         """
         """
-     
+
 	 def maxContactID(self):
 		"""Returns the value of the highest ContactID on the DB."""
 		pass
-  
 
     def __del__(self):
         self.conn.commit()
         self.conn.close()
+
+class FabDatabaseTest(unittest.TestCase):
+    """Unittesting database management."""
+
+    def setUp(self):
+        self.db = FabDatabase("test.db")
+
+    def test_insert(self):
+        """Testing row insertion."""
+        pass
+
+    def test_remove(self):
+        """Testing row removal."""
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
