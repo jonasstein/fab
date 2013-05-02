3b0231d6cc065014650567c1cee40341611d1a78
Elias Rabel <ylikx.0@gmail.com>
2013-05-02 21:49:34 +0200

    Forgot to commit changes in addressbook.py and fab.py

--- fsql.py
+++ fsql.py
@@ -8,7 +8,8 @@ Handles low level DB instructions.
 import sqlite3
 import sys
 
-import unittest
+class FabDatabaseError:
+    pass
 
 class FabDatabase:
     """Class uppercase first letter, methods lowercase, underscore
@@ -18,8 +19,8 @@ class FabDatabase:
         """Connects to the given database. If such database does not exist, it will be created together with an empty table representid the data and another for address book properties mirroring a dictionary."""
 
         try:
-            self.conn = sqlite3.connect(filename)
-            self.conn.execute("""CREATE TABLE IF NOT EXISTS Data(
+            self._conn = sqlite3.connect(filename)
+            self._conn.execute("""CREATE TABLE IF NOT EXISTS Data(
 								ID INTEGER PRIMARY KEY,
 								ContactID INTEGER,
 								Type TEXT,
@@ -28,45 +29,45 @@ class FabDatabase:
 								Value TEXT,
 								TimeStamp TEXT,
 								Comment TEXT)""")
-            self.conn.execute("""CREATE TABLE IF NOT EXISTS Properties(
+            self._conn.execute("""CREATE TABLE IF NOT EXISTS Properties(
                                 Key TEXT,
                                 Value TEXT)""")
-            self.conn.commit()
+            self._conn.commit()
 
         except sqlite3.OperationalError:
             print("OperationalError")
+            raise FabDatabaseError
 
         except sqlite3.DatabaseError:
             print("DatabaseError: file is encrypted or is not a database")
-
-        except RuntimeError:
-            print("Runtime Error: Could not create new database.")
+            raise FabDatabaseError
 
     def __del__(self):
-        self.conn.commit()
-        self.conn.close()
+        self._conn.commit()
+        self._conn.close()
 
     def delete_DB(self):
         """Delete the database. Close first.
         """
+        pass
 
     def store_row(self, registry):
         """Inserts a registry from the data table. Registry must be a tuple.
         """
-        self.conn.execute("INSERT INTO Data VALUES (?,?,?,?,?,?,?)", registry)
-        self.conn.commit()
+        self._conn.execute("INSERT INTO Data VALUES (?,?,?,?,?,?,?,?)", registry)
+        self._conn.commit()
 
     def erase_row(self,ID):
         """Erases a row.
         """
-        self.conn.execute( "DELETE FROM Data WHERE ID = ?", [ID,])
-        self.conn.commmit()
+        self._conn.execute( "DELETE FROM Data WHERE ID = ?", [ID,])
+        self._conn.commmit()
 
     def erase_contact(self, ContactID):
         """Removes all the entries associated with the given ContactID."""
 
-        self.conn.execute("DELETE FROM Data WHERE ContactID = ?", [ContactID,])
-        self.conn.commit()
+        self._conn.execute("DELETE FROM Data WHERE ContactID = ?", [ContactID,])
+        self._conn.commit()
 
     def read(self):
         """given a set of attr. and ID's return such attr. of ID's
@@ -81,19 +82,3 @@ class FabDatabase:
         pass
 
 
-class FabDatabaseTest(unittest.TestCase):
-    """Unittesting database management."""
-
-    def setUp(self):
-        self.db = FabDatabase("test.db")
-
-    def test_insert(self):
-        """Testing row insertion."""
-        pass
-
-    def test_remove(self):
-        """Testing row removal."""
-        pass
-
-if __name__ == "__main__":
-    unittest.main()
