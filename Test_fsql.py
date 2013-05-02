33c9a2464f5844304df1113fdc2ea09a8b3ff088
mai01 <maike.schoen@nf.mpg.de>
2013-05-02 23:47:25 +0200

    tests added for store_row and get_row

--- Test_fsql.py
+++ Test_fsql.py
@@ -15,19 +15,26 @@ class FabDatabaseTest(unittest.TestCase):
 
     def setUp(self):
         self.db = FabDatabase(TESTDATABASE)
+        self.dic={"ID":1,"ContactID":2,"Type":'Name',"Valid":1,"Timestamp":'10-10-2013T010101',"Comment":'blub'}
 
-    def test_insert(self):
-        """Testing row insertion."""
-        pass
+   # def test_insert(self):
+   #     """Testing row insertion."""
+    #    pass
 
-    def test_remove(self):
-        """Testing row removal."""
-        pass
+#    def test_remove(self):
+#        """Testing row removal."""
+#        pass
 
     def tearDown(self):
         """Cleanup after tests."""
         os.unlink(TESTDATABASE)
-        pass
+    
+    def test_store_row(self):
+        self.assertEqual(self.db.store_row(self.dic),None)
+
+    def test_get_row(self):
+        dic2 = self.db.get_row(1)
+        self.assertEqual(dic2, self.dic)
 
 if __name__ == "__main__":
     unittest.main()
